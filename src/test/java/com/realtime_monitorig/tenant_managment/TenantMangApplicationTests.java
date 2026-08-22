215b80dd02e834e78077e3f86505ae6e9308937e
ChedlyRebai <chedly.rebai123@gmail.com>
2026-08-22 01:43:03 +0100

    update tenant

--- src/test/java/com/realtime_monitorig/tenant_managment/TenantMangApplicationTests.java
+++ src/test/java/com/realtime_monitorig/tenant_managment/TenantMangApplicationTests.java
@@ -2,8 +2,10 @@ package com.realtime_monitorig.tenant_managment;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(properties = {"grpc.server.enabled=false"})
+@ActiveProfiles("test")
 class TenantMangApplicationTests {
 
 	@Test
